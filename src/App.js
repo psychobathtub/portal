6db472f0d326c15daa0c4418eb18fb6c743944c9
psychobathtub <126437862+psychobathtub@users.noreply.github.com>
2023-08-12 17:41:47 -0400

    gh-page setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import ContestantForm from './routes/ContestantForm.tsx';
 import About from './routes/About.tsx';
@@ -12,9 +12,9 @@ import logo from './img/logo.png';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
   
-  <nav className="flex items-center justify-between flex-wrap bg-yellow-400 p-6">
+    <nav className="flex items-center justify-between flex-wrap bg-yellow-400 p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         {/* <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg> */}
         <span className="font-semibold text-xl tracking-tight"><a href="/"><img src={logo} className="h-8 mr-3"/></a></span>
@@ -43,14 +43,14 @@ const App = () => {
       <div className="App" >
 
         <Routes>
-          <Route path="/" element={<Home/>}/>
           <Route path="/guidelines" element={<Guidelines/>}/>
           <Route path="/submit" element={<ContestantForm/>}/>
           <Route path="/about" element={<About/>} />
+          <Route exact path="/" element={<Home/>}/>
         </Routes>
 
       </div>
-    </BrowserRouter>
+      </HashRouter>
   );
 };
 
