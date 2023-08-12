9ad43945ed8528787da3c09ed33b0d344701a09d
psychobathtub <126437862+psychobathtub@users.noreply.github.com>
2023-08-12 18:11:08 -0400

    gh-page setup - Troubleshooting

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
   <nav className="flex items-center justify-between flex-wrap bg-yellow-400 p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         {/* <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg> */}
-        <span className="font-semibold text-xl tracking-tight"><a href="/"><img src={logo} className="h-8 mr-3"/></a></span>
+        <span className="font-semibold text-xl tracking-tight"><a href="/portal"><img src={logo} className="h-8 mr-3"/></a></span>
       </div>
       <div className="block lg:hidden">
         <button className="flex items-center px-3 py-2 border rounded text-yellow-200 border-yellow-400 hover:text-white hover:border-white">
@@ -26,15 +26,15 @@ const App = () => {
       </div>
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div className="text-sm lg:flex-grow">
-          <a href="/portal/about" className="block mt-4 lg:inline-block lg:mt-0 text-yellow-100 hover:text-white mr-4">
+          <a href="/about" className="block mt-4 lg:inline-block lg:mt-0 text-yellow-100 hover:text-white mr-4">
             About
           </a>
-          <a href="/portal/guidelines" className="block mt-4 lg:inline-block lg:mt-0 text-yellow-100 hover:text-white mr-4">
+          <a href="/guidelines" className="block mt-4 lg:inline-block lg:mt-0 text-yellow-100 hover:text-white mr-4">
             Guidelines
           </a>
         </div>
         <div>
-          <a href="/portal/submit" className="inline-block text-sm px-4 py-2 leading-none border rounded text-yellow-100 border-yellow-100 hover:border-transparent hover:text-yellow-500 hover:bg-white mt-4 lg:mt-0">Submit</a>
+          <a href="/submit" className="inline-block text-sm px-4 py-2 leading-none border rounded text-yellow-100 border-yellow-100 hover:border-transparent hover:text-yellow-500 hover:bg-white mt-4 lg:mt-0">Submit</a>
         </div>
       </div>
 
@@ -44,9 +44,9 @@ const App = () => {
 
         <Routes>
           <Route path="/portal" element={<Home/>}/>
-          <Route path="/portal/guidelines" element={<Guidelines/>}/>
-          <Route path="/portal/submit" element={<ContestantForm/>}/>
-          <Route path="/portal/about" element={<About/>} />
+          <Route path="/guidelines" element={<Guidelines/>}/>
+          <Route path="/submit" element={<ContestantForm/>}/>
+          <Route path="/about" element={<About/>} />
         </Routes>
 
       </div>
